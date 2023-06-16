490c6395611aa7cb79149f0be995eaaf79cbb0cf
marcosd <marcosdiogo.eng@outlook.com>
2023-06-16 00:39:16 -0300

    iniciando um projeto Java Spring Boot 3

---
+++ screenmatch/src/main/java/br/com/projeto/screenmatch/controller/FilmeController.java
@@ -0,0 +1,19 @@
+package br.com.projeto.screenmatch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("filmes")
+public class FilmeController {
+
+    @GetMapping
+    public String carregaPaginaFormulario(){
+        return "filmes/formulario";
+    }
+    @PostMapping
+    public void salvaDadosFilmes(){
+    }
+}
