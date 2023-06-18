4220b2c4855beedffdb53f8d0f0bc3c225104577
marcosd <marcosdiogo.eng@outlook.com>
2023-06-18 13:58:28 -0300

    novas alterações + css

--- screenmatch/src/main/java/br/com/projeto/screenmatch/controller/FilmeController.java
+++ screenmatch/src/main/java/br/com/projeto/screenmatch/controller/FilmeController.java
@@ -3,6 +3,7 @@ package br.com.projeto.screenmatch.controller;
 import br.com.projeto.screenmatch.domain.filme.DadosCadastroFilme;
 import br.com.projeto.screenmatch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +16,20 @@ import java.util.List;
 public class FilmeController {
 
     private List<Filme> filmes = new ArrayList<>();
-    @GetMapping
+    @GetMapping("/formulario")
     public String carregaPaginaFormulario(){
         return "filmes/formulario";
+    }
+    @GetMapping
+    public String carregaPaginaListagem(Model model){
+        model.addAttribute("lista", filmes);
+        return "filmes/listagem";
     }
     @PostMapping
     public String cadastraFilme(DadosCadastroFilme dados){
         var filme = new Filme(dados);
         filmes.add(filme);
-        System.out.println(filmes);
-        return "filmes/formulario";
+        return "redirect:/filmes";
 
     }
 }
